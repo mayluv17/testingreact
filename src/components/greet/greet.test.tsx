86346327ae589c5c520fe55f35fd5e17e3e69f8f
“mayluv17” <mayowaibrahim009@gmail.com>
2022-12-10 19:32:17 +0100

    test coverage

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
-test("great renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/Hello/i);
-  expect(textElement).toBeInTheDocument();
-});
+//test.only test.skip describe.skip describe.only
 
-test("greet with name", () => {
-  render(<Greet name="mayowa" />);
-  const textElement = screen.getByText(/Hello/i);
-  expect(textElement).toBeInTheDocument();
+describe("greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/Hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("with name", () => {
+    render(<Greet name="mayowa" />);
+    const textElement = screen.getByText(/Hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 });
